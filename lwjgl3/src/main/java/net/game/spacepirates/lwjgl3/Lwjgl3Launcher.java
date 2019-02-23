061218900906bb3c7d95c760877e121863c41dfd
Nick Guy <nick.guy@hotmail.co.uk>
2019-02-23 16:43:59 +0000

    Initial commit of project

---
+++ lwjgl3/src/main/java/net/game/spacepirates/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,24 @@
+package net.game.spacepirates.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import net.game.spacepirates.SpacePiratesLauncher;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new SpacePiratesLauncher(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("SpacePirates");
+        configuration.setWindowedMode(640, 480);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+        return configuration;
+    }
+}
