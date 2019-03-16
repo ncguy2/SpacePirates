06b9728a78e418a1678fdf1bcb22978c10365a9b
Nick Guy <nick.guy@hotmail.co.uk>
2019-03-16 19:29:16 +0000

    More progress on GPU particle systems

--- core/src/main/java/net/game/spacepirates/particles/system/AbstractParticleSystem.java
+++ core/src/main/java/net/game/spacepirates/particles/system/AbstractParticleSystem.java
@@ -1,22 +1,61 @@
 package net.game.spacepirates.particles.system;
 
-import net.game.spacepirates.particles.ParticleBuffer;
-import net.game.spacepirates.particles.ParticleProfile;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+import com.badlogic.gdx.graphics.glutils.ShaderProgram;
+import com.badlogic.gdx.math.Matrix3;
+import net.game.spacepirates.particles.*;
+import net.game.spacepirates.services.Services;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class AbstractParticleSystem {
 
-    private ParticleProfile profile;
-    private ParticleBuffer buffer;
-    private List<Integer> particleIndices;
+    protected final Map<String, Consumer<Integer>> uniformSetters;
+    protected final List<Consumer<ShaderProgram>> uniformTasks;
+    protected final Map<String, String> macroParams;
+
+    protected ParticleProfile profile;
+    protected ParticleBuffer buffer;
+    protected List<Integer> particleIndices;
+    protected Supplier<Matrix3> spawnMatrixSupplier;
+    protected ParticleShader spawnShader;
+    protected ParticleShader updateShader;
 
     public AbstractParticleSystem(ParticleProfile profile, ParticleBuffer buffer) {
         this.profile = profile;
         this.buffer = buffer;
         this.particleIndices = new ArrayList<>();
+
+        this.uniformSetters = new HashMap<>();
+        this.uniformTasks = new ArrayList<>();
+        this.macroParams = new HashMap<>();
+
+        FileHandle fileHandle = Gdx.files.internal("particles/skeleton.comp");
+
+        spawnShader = new ParticleShader(profile.name + ", Spawn shader", fileHandle, macroParams);
+        updateShader = new ParticleShader(profile.name + ", Update shader", fileHandle, macroParams);
+
+        ParticleService particleService = Services.get(ParticleService.class);
+        for (String block : profile.blocks) {
+            particleService.getParticleBlock(block).ifPresent(this::addBlock);
+        }
+
+        spawnShader.reloadImmediate();
+        updateShader.reloadImmediate();
+    }
+
+    public void addBlock(ParticleBlock block) {
+        addBlock(block, block.type);
+    }
+
+    protected void addBlock(ParticleBlock block, ParticleBlock.Type type) {
+        switch(type) {
+            case Spawn:  spawnShader.addBlock(block);  break;
+            case Update: updateShader.addBlock(block); break;
+        }
     }
 
     public void setParticleIndices(int... indices) {
@@ -25,6 +64,10 @@ public class AbstractParticleSystem {
 
     public void killParticles(int... indices) {
         Arrays.stream(indices).forEach(particleIndices::remove);
+    }
+
+    public void fetchParticleIndices(int amount) {
+        setParticleIndices(Services.get(ParticleService.class).issueIndices(amount));
     }
 
     public int[] getIndices() {
